788ea607007aef2043adc081f84212fd7fe9bcde
Sky Hong <sky9109@gmail.com>
2026-07-07 14:39:42 +0800

    Publish site snapshot 2026-07-07T14:39:42+08:00

--- data/public-calendar-events.js
+++ data/public-calendar-events.js
@@ -1,6 +1,6 @@
 window.publicCalendarEvents = {
   "version": 1,
-  "generatedAt": "2026-07-07T14:04:29+08:00",
+  "generatedAt": "2026-07-07T14:39:30+08:00",
   "timezone": "Asia/Taipei",
   "count": 4,
   "source": "/api/events.json",
