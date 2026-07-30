4966a71342b9ba94a10e250a2941462d23b96ca5
Sky Hong <sky9109@gmail.com>
2026-07-31 00:13:11 +0800

    Publish requested harmonica sources

--- data/score-data.js
+++ data/score-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORES = {
   "count": 797,
-  "generatedAt": "2026-07-30T23:56:18+08:00",
+  "generatedAt": "2026-07-31T00:08:43+08:00",
   "scores": [
     {
       "arranger": "陳瑤容",
