8c26f16ebe4750be1480edccf15a22438c67239f
Sky Hong <sky9109@gmail.com>
2026-08-07 03:09:44 +0800

    Publish site snapshot 2026-08-07T03:09:44+08:00

--- data/score-data.js
+++ data/score-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORES = {
   "count": 797,
-  "generatedAt": "2026-08-07T02:03:29+08:00",
+  "generatedAt": "2026-08-07T03:08:51+08:00",
   "scores": [
     {
       "arranger": "陳瑤容",
