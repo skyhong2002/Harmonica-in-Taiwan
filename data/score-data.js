b93dc769d9016a52c2a983730c39953c9476a879
Sky Hong <sky9109@gmail.com>
2026-08-06 22:44:44 +0800

    Publish site snapshot 2026-08-06T22:44:44+08:00

--- data/score-data.js
+++ data/score-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORES = {
   "count": 797,
-  "generatedAt": "2026-08-06T21:36:56+08:00",
+  "generatedAt": "2026-08-06T22:43:53+08:00",
   "scores": [
     {
       "arranger": "陳瑤容",
--- data/score-source-data.js
+++ data/score-source-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORE_SOURCES = {
   "count": 39,
-  "generatedAt": "2026-08-06T21:36:56+08:00",
+  "generatedAt": "2026-08-06T22:43:53+08:00",
   "scoreSources": [
     {
       "arranger": "",
