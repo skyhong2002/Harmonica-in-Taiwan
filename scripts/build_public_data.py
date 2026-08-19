5877c39d2d21faabf023782818196b8b68dccc5e
Sky Hong <sky9109@gmail.com>
2026-08-20 04:51:20 +0800

    Expand current harmonica education sources

--- scripts/build_public_data.py
+++ scripts/build_public_data.py
@@ -32,6 +32,8 @@ CURATED_SOURCE_AVATARS = {
     "SUZUKI Harmonica": "suzuki-harmonica.png",
     "C.A. SEYDEL SÖHNE": "seydel-harmonica.png",
     "SHG Hering Harmonicas": "hering-harmonica.png",
+    "香港兒童合唱團口琴課程": "hkcc-harmonica.png",
+    "基隆社區大學就是吹口琴": "keelung-community-harmonica.jpg",
 }
 TAIPEI_TZ = timezone(timedelta(hours=8))
 AVATAR_PLATFORM_PRIORITY = {
