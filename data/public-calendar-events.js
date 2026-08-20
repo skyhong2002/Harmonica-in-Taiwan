d6a8b718c011b10e2c9d6d748134511d8d22b926
Sky Hong <sky9109@gmail.com>
2026-08-21 03:41:39 +0800

    Publish site snapshot 2026-08-21T03:41:39+08:00

--- data/public-calendar-events.js
+++ data/public-calendar-events.js
@@ -1,6 +1,6 @@
 window.publicCalendarEvents = {
   "version": 2,
-  "generatedAt": "2026-08-21T02:32:57+08:00",
+  "generatedAt": "2026-08-21T03:40:40+08:00",
   "timezone": "Asia/Taipei",
   "calendarType": "taiwan_physical",
   "count": 9,
