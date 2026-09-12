ead8ff0bce00a3e76e878fe6f244b134a093cd38
Sky Hong <sky9109@gmail.com>
2026-09-12 10:15:17 +0800

    Publish site snapshot 2026-09-12T10:15:17+08:00

--- data/site-data.js
+++ data/site-data.js
@@ -3848,7 +3848,7 @@ window.HARMONICA_OBSERVE_DATA = {
       "latestUpdateLocal": "2026-09-02 00:47",
       "latestUpdateSource": "王心禾",
       "latestUpdateUrl": "https://www.instagram.com/stories/harmonica_wang/3976713968781835072/",
-      "avatarUrl": "/assets/source-avatars/dd15f025e5d56a25901e.webp",
+      "avatarUrl": "/assets/source-avatars/f7c087297a9a30d9e110.webp",
       "sourceInitials": "王心",
       "sourceTags": [
         "演奏者",
