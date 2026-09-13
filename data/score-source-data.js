0ee997e9fc93a20fe8b7f13163e43c86c351ba82
Sky Hong <sky9109@gmail.com>
2026-09-13 20:58:01 +0800

    Publish site snapshot 2026-09-13T20:58:01+08:00

--- data/score-source-data.js
+++ data/score-source-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORE_SOURCES = {
   "count": 39,
-  "generatedAt": "2026-09-13T20:25:18+08:00",
+  "generatedAt": "2026-09-13T20:56:04+08:00",
   "scoreSources": [
     {
       "arranger": "",
--- data/site-data.js
+++ data/site-data.js
@@ -2123,7 +2123,7 @@ window.HARMONICA_OBSERVE_DATA = {
       "latestUpdateLocal": "2026-09-09 12:00",
       "latestUpdateSource": "新竹縣文化局",
       "latestUpdateUrl": "https://www.youtube.com/watch?v=za_f0MK36_0",
-      "avatarUrl": "/assets/source-avatars/ff480a873dd7e7315b46.webp",
+      "avatarUrl": "/assets/source-avatars/1b8db3b814cbd6da6490.webp",
       "sourceInitials": "新竹",
       "sourceTags": [
         "場館平台"
