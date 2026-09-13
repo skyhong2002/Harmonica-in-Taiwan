80fa9f75831b0c4603b99dc2e1231fe85fe35cf8
Sky Hong <sky9109@gmail.com>
2026-09-13 19:53:37 +0800

    Publish site snapshot 2026-09-13T19:53:37+08:00

--- data/score-data.js
+++ data/score-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORES = {
   "count": 797,
-  "generatedAt": "2026-09-13T19:20:53+08:00",
+  "generatedAt": "2026-09-13T19:51:48+08:00",
   "scores": [
     {
       "arranger": "陳瑤容",
--- data/site-data.js
+++ data/site-data.js
@@ -6774,7 +6774,7 @@ window.HARMONICA_OBSERVE_DATA = {
       "latestUpdateLocal": "2026-08-31 12:35",
       "latestUpdateSource": "Timothy Yip 葉進禧",
       "latestUpdateUrl": "https://www.instagram.com/stories/timothyyip_harmonica/3975620789713580597/",
-      "avatarUrl": "/assets/source-avatars/b5a73b089c3c8a6c0406.webp",
+      "avatarUrl": "/assets/source-avatars/1aa89ba3437d026e6a10.webp",
       "sourceInitials": "TY",
       "sourceTags": [
         "演奏者",
