270d11709e4418aa65375333deb4c01d97e609be
Sky Hong <sky9109@gmail.com>
2026-09-14 12:31:16 +0800

    Publish site snapshot 2026-09-14T12:31:16+08:00

--- data/public-calendar-events.js
+++ data/public-calendar-events.js
@@ -1,6 +1,6 @@
 window.publicCalendarEvents = {
   "version": 2,
-  "generatedAt": "2026-09-14T12:03:52+08:00",
+  "generatedAt": "2026-09-14T12:30:38+08:00",
   "timezone": "Asia/Taipei",
   "calendarType": "taiwan_physical",
   "count": 8,
