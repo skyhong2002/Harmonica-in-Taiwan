139b878d21a0aaaba60cee4154bdbabe48fe323e
Sky Hong <sky9109@gmail.com>
2026-09-16 22:26:30 +0800

    Publish site snapshot 2026-09-16T22:26:30+08:00

--- data/score-source-data.js
+++ data/score-source-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORE_SOURCES = {
   "count": 39,
-  "generatedAt": "2026-09-16T22:17:19+08:00",
+  "generatedAt": "2026-09-16T22:24:38+08:00",
   "scoreSources": [
     {
       "arranger": "",
