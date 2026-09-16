8dbcaa0c7c47bb02fcc41e2efdecef55f1bd415c
Sky Hong <sky9109@gmail.com>
2026-09-16 09:13:37 +0800

    Publish site snapshot 2026-09-16T09:13:37+08:00

--- data/site-data.js
+++ data/site-data.js
@@ -7685,7 +7685,7 @@ window.HARMONICA_OBSERVE_DATA = {
       "latestUpdateLocal": "2026-08-30 21:48",
       "latestUpdateSource": "蔡明憲",
       "latestUpdateUrl": "https://www.instagram.com/stories/damingtsai/3975174060992244965/",
-      "avatarUrl": "/assets/source-avatars/6cd5815ea68316e4a3ff.webp",
+      "avatarUrl": "/assets/source-avatars/1c9b1ac0cf055d33f82b.webp",
       "sourceInitials": "蔡明",
       "sourceTags": [
         "演奏者",
