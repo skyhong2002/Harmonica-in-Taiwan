65e1a2c89dbd1d4d480ae5bd21804460da92f2d3
Sky Hong <sky9109@gmail.com>
2026-09-17 17:57:08 +0800

    Publish site snapshot 2026-09-17T17:57:08+08:00

--- data/public-calendar-events.js
+++ data/public-calendar-events.js
@@ -1,6 +1,6 @@
 window.publicCalendarEvents = {
   "version": 2,
-  "generatedAt": "2026-09-17T17:46:38+08:00",
+  "generatedAt": "2026-09-17T17:56:30+08:00",
   "timezone": "Asia/Taipei",
   "calendarType": "taiwan_physical",
   "count": 9,
--- data/score-source-data.js
+++ data/score-source-data.js
@@ -1,6 +1,6 @@
 window.HARMONICA_OBSERVE_SCORE_SOURCES = {
   "count": 39,
-  "generatedAt": "2026-09-17T17:45:43+08:00",
+  "generatedAt": "2026-09-17T17:55:40+08:00",
   "scoreSources": [
     {
       "arranger": "",
