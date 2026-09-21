b7d2a6f783f4171398ec67aef0b46a55300fc375
Sky Hong <sky9109@gmail.com>
2026-09-21 16:07:43 +0800

    Publish site snapshot 2026-09-21T16:07:43+08:00

--- data/site-data.js
+++ data/site-data.js
@@ -5287,7 +5287,7 @@ window.HARMONICA_OBSERVE_DATA = {
       "latestUpdateLocal": "2026-09-09 00:05",
       "latestUpdateSource": "口琴保存與推廣協會 SPAH",
       "latestUpdateUrl": "https://www.facebook.com/SPAH63/posts/pfbid02dFoMhGmnzST4FXABL15S7JXcnew8otCiuXMEX2uC4d7w91abEG6PELeyqBUk8mH6l",
-      "avatarUrl": "/assets/source-avatars/a2aadf54b966c7a0ef90.webp",
+      "avatarUrl": "/assets/source-avatars/bdef6974d1d1a5aa2337.webp",
       "sourceInitials": "S",
       "sourceTags": [
         "協會",
